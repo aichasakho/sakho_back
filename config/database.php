9b56e7a5e66f7594b05c405a753ab773e2d863e2
aichasakho <aichasakho2205@gmail.com>
2024-12-18 21:44:02 +0000

    modif

--- config/database.php
+++ config/database.php
@@ -48,14 +48,11 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-<<<<<<< HEAD
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-=======
-            'database' => env('DB_DATABASE', 'SakhoImmo'),
+            /*'database' => env('DB_DATABASE', 'SakhoImmo'),
             'username' => env('DB_USERNAME', 'root'),
->>>>>>> master
-            'password' => env('DB_PASSWORD', ''),
+            */'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
